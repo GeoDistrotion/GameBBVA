291ae4a0fee4eb4d5fc080413e7806cbef60e3c1
Geo <geo.distor@gmail.com>
2021-03-16 05:01:09 -0600

    Implement the bullet class

---
+++ src/js/classes/Bullet.js
@@ -0,0 +1,28 @@
+export default class Bullet{
+    constructor( x, y, dir = 'right', context){
+        this.x = x;
+        this.y = y;
+        this.radius = 5;
+        this.direction = dir
+        this.c  = context;
+    }
+
+    draw(){
+        this.c.beginPath();
+        this.c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        this.c.fillStyle = 'red';
+        this.c.fill();
+    }
+
+    setPosX(x){
+        this.x = x;
+    } 
+
+    gettPosX(x){
+        return this.x;
+    }
+
+    getDirection(){
+        return this.direction;
+    }
+}
--- src/js/classes/CanvasGame.js
+++ src/js/classes/CanvasGame.js
@@ -1,115 +1,154 @@
+import Bullet from './Bullet';
 import Player from './Player';
 
 export default class CanvasGame{
     constructor( assets ){
-        this.gameCont = document.querySelector('#InitBBVAGame');
-        this.canvas = document.createElement('canvas');
-        this.assets = assets;
-        this.context;
-        this.hero;
-        this.floor;
-        this.center;
-        this.contWidth = 0;
-        this.contHeight = 0;
-        this.zombies = [];
-        this.zDir = "right";
-        this.zombieloop;
+        this.gameCont = document.querySelector('#InitBBVAGame'); // Get the main container
+        this.canvas = document.createElement('canvas'); // Init element canvas
+        this.assets = assets;  // Get the game assets preloaded
+        this.context; // 2D CONTEXT
+        this.hero; // Hero cont
+        
+        this.floor; // Floor parameter
+        this.center; // Center param
+        
+        this.contWidth = 0; // Main container width
+        this.contHeight = 0; // Main container height
+
+        this.animate; // Set the window animation frame id
+        
+        this.zombies = []; // Array to store the zombies objects
+        this.zDir = "right"; // Set the current zombie direction
+        this.zombieloop; // Set an interval id
+
+        this.bullets = []; // Array to store the bullets objects
     }
 
-    startCanvasGame(){
-        this.context = this.canvas.getContext('2d');
+    startCanvasGame(){ // Trigger the start game  && set the global conf
+        this.context = this.canvas.getContext('2d'); // Init the cotext
         
-        this.contWidth = this.gameCont.offsetWidth;
-        this.contHeight = this.gameCont.offsetHeight;
+        this.contWidth = this.gameCont.offsetWidth; // set the maib cont width
+        this.contHeight = this.gameCont.offsetHeight; // set the main cont height
         
-        this.canvas.width = this.contWidth;
-        this.canvas.height = this.contHeight;
+        this.canvas.width = this.contWidth; // set the canvas width
+        this.canvas.height = this.contHeight; // set the canvas height
         
-        this.floor = this.contHeight - 150;
-        this.center = this.contWidth / 2;
+        this.floor = this.contHeight - 150; // set the game floor 
+        this.center = this.contWidth / 2; // set the center game
         
-        this.gameCont.appendChild(this.canvas);
-        this.animate;
+        this.gameCont.appendChild(this.canvas); // inject the canvas in the main container
         
-        this.hero = new Player(this.assets.hero_right, this.assets.hero_left, 50, this.context, 'Hero');
-        this.hero.setPosX( this.center );
-        this.hero.setPosY( this.floor - 140 );
+        this.hero = new Player(this.assets.hero_right, this.assets.hero_left, 50, this.context, 'Hero'); // Create the hero object
+        this.hero.setPosX( this.center ); // set the init hero x pos
+        this.hero.setPosY( this.floor - 140 ); // set the init hero y pos 
 
-        this._zombiesmanager();
-        this._addHeroControls();
-        this._turnOnEngine();
+        this._zombiesmanager(); // start the zombie manager creator
+        this._addHeroControls(); // add keyboard events to the hero object
+        this._turnOnEngine(); // start the game loop engine;
     }
 
     _addHeroControls(){
-        addEventListener('keydown', (e)=>{
-            const hPosX= this.hero.getPosX();
-            if(e.key === 'ArrowLeft'){
-                this.hero.setDirection('left');
-                this.hero.startRun();
-                if(hPosX > 0){
-                    this.hero.setPosX(hPosX - 10);
-                }
-            }
-            else if(e.key === 'ArrowRight'){
-                this.hero.startRun();
-                this.hero.setDirection('right');
-                if(hPosX < this.contWidth - 100){
-                    this.hero.setPosX(hPosX + 10);
-                }
+        addEventListener('keydown', (e)=>{ // adding the window keyboard event
+            const hPosX = this.hero.getPosX(); // get the hero current x pos
+            const hDir = this.hero.getDirection(); // get the hero current direction
+            const bulletXOrigin = hDir=='right'? hPosX + 80: hPosX - 20;  // set the origin of the new bullet;
+            switch (e.key) {
+                case "ArrowLeft": // catch the Arrow left
+                    this.hero.setDirection('left'); // set the hero direction to left
+                    this.hero.startRun(); // init the run animation to the direction before configured
+                    if(hPosX > 0){ // limit the environment to left
+                        this.hero.setPosX(hPosX - 10);
+                    }    
+                    break;
+                case "ArrowRight": // catch the arrow right 
+                    this.hero.setDirection('right'); // set the hero direction to right
+                    this.hero.startRun(); // init the run animation to the direction before configured
+                    if(hPosX < this.contWidth - 100){ // limit the environment to right
+                        this.hero.setPosX(hPosX + 10);
+                    }
+                break;
+                case " ": // catch the spacebar
+                    const newBullet = new Bullet(bulletXOrigin, this.contHeight - 180, hDir,this.context); // create the new bullet
+                    this.bullets.push(newBullet); // add the new bullet to the bullets array
+                break;
+                default:
+                    break;
             }
         })
-        addEventListener('keyup', e => {
-            if(e.key === 'ArrowLeft'){
-                this.hero.stopRun();
-            }
-            else if(e.key === 'ArrowRight'){
-                this.hero.stopRun();
+        addEventListener('keyup', e => { // catch the key up event
+            switch (e.key) {
+                case 'ArrowLeft':
+                    this.hero.stopRun(); // stop the run left animation
+                    break;
+                case 'ArrowRight':
+                    this.hero.stopRun(); // stop the run right animation
+                    break;
+            
+                default:
+                    break;
             }
         })
     }
 
     _zombiesmanager(){
-        this.zombieloop = setInterval(() => {
-            if(this.zombies.length < 2){
-                let newZombie = new Player(this.assets.zombie_right,this.assets.zombie_left, 10, this.context, "zombie", this.zDir);
-                newZombie.setPosY( this.floor - 140 );
-                newZombie.setPosX( (this.zDir === "right")? -50 : (this.contWidth + 50) );
-                this.zDir = (this.zDir === "right")? "left" : "right";
-                console.log(newZombie.getDirection(), 'Zombie direction');
-                this.zombies.push(newZombie);
+        this.zombieloop = setInterval(() => { // start the time interval and add it to the id
+            if(this.zombies.length < 3){
+                let newZombie = new Player(this.assets.zombie_right,this.assets.zombie_left, 10, this.context, "zombie", this.zDir); // create zombie object
+                newZombie.setPosY( this.floor - 140 ); // set the zombir Y pos origin
+                newZombie.setPosX( (this.zDir === "right")? -50 : (this.contWidth + 50) ); // set the zombir X pos origin
+                this.zDir = (this.zDir === "right")? "left" : "right"; // set the Zombie move direction
+                this.zombies.push(newZombie); // add the new zombie to the main zombies array
             }
         }, 3000);
     }
 
     _turnOnEngine(){
-        this.context.clearRect(0,0,this.contWidth, this.contWidth);
-        this.context.fillStyle = 'black';
-        this.context.fillRect(0, this.floor, this.contWidth, 150);
-        this.hero.draw();
+        this.context.clearRect(0,0,this.contWidth, this.contWidth); // clean the stage each iteration
+        this.context.fillStyle = 'black'; // set the game floor color
+        this.context.fillRect(0, this.floor, this.contWidth, 150); // draw the game floor
+        this.hero.draw(); // draw the hero object each iteration
         
-        this.zombies.forEach((zombie, z) => {
-            zombie.draw();
-            let zPosX = zombie.getPosX();
-            const cirrDir = zombie.getDirection();
+        this.zombies.forEach((zombie, z) => { // read each zombie object from the zombie list
+            zombie.draw(); // draw the zombie object each iteration
+            const zPosX = zombie.getPosX(); // get the current X pos
+            const currentZombieDir = zombie.getDirection(); // get the current direction
 
-            if( cirrDir === "right" && zPosX < this.contWidth ){
+            if( currentZombieDir === "right" && zPosX < this.contWidth ){ // limit the area
                 zombie.setPosX(zPosX + 2);
-            }else if( cirrDir === "right" && zPosX >= this.contWidth){
-                this.zombies.splice(z, 1);
+            }else if( currentZombieDir === "right" && zPosX >= this.contWidth){
+                this.zombies.splice(z, 1); // remove the object from the list
             }
             
-            if( zombie.getDirection() === "left" && zPosX > -100){
+            if( zombie.getDirection() === "left" && zPosX > -100){ // limit the area
                 zombie.setPosX(zPosX - 2);
-            }else if( cirrDir === "left" && zPosX <= 0){
-                this.zombies.splice(z, 1);
+            }else if( currentZombieDir === "left" && zPosX <= 0){
+                this.zombies.splice(z, 1);// remove the object from the list
             }
 
-            zombie.startRun();
+            zombie.startRun(); // start the animation depending on the zombie direction
         });
-        this.animate = requestAnimationFrame( () => this._turnOnEngine() );
+
+        this.bullets.forEach((bullet, b)=>{
+            bullet.draw(); // draw the bullet object each iteration
+            const bPosX = bullet.gettPosX(); 
+            const currentBulletDir = bullet.getDirection();
+            if(currentBulletDir == 'right' && bPosX < this.contWidth ){
+                bullet.setPosX( bPosX + 3 );
+            }else if( currentBulletDir === "right" && bPosX >= this.contWidth){
+                this.bullets.splice(b, 1);
+            }
+            if(currentBulletDir == 'left' && bPosX > 0){
+                bullet.setPosX( bPosX - 3 );   
+            }else if( currentBulletDir === "left" && bPosX <= 0){
+                this.bullets.splice(b, 1);
+            }
+        });
+
+        this.animate = requestAnimationFrame( () => this._turnOnEngine() ); // recursive iteration function
     }
     
     stopCanvasGame(){
-        cancelAnimationFrame(this.animate);
+        cancelAnimationFrame(this.animate); // cancel the animation loop engine
+        clearInterval(this.zombieloop); // clear the time intervar zombie creator
     }
 }
